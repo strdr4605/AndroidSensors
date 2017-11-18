4df09e50012f038bb53eb1b61127442110289f12
strdr4605 <str.dr4605@gmail.com>
2017-11-18 16:18:00 +0100

    Add RecyclerView to activity_main, implement SensorsAdapter Class on Wear

--- wear/src/main/java/com/example/strainu/androidsensors/MainActivity.kt
+++ wear/src/main/java/com/example/strainu/androidsensors/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.strainu.androidsensors
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.wearable.activity.WearableActivity
+import android.support.wear.widget.WearableRecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_list.layoutManager = LinearLayoutManager(this)
+        recycler_list.setHasFixedSize(true)
+
+        val myDataset= Array(20, {x -> "Sensor$x"})
+        recycler_list.adapter = SensorsAdapter(myDataset)
 
         // Enables Always-on
         setAmbientEnabled()
